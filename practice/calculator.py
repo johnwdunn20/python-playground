d56f579ec0de1ef7372e3610439a1f403b93e64b
John Dunn <johnwdunn20@gmail.com>
2024-04-03 10:31:24 -0700

    changes

--- practice/calculator.py
+++ practice/calculator.py
@@ -1,7 +1,26 @@
 print('__name__:', __name__)
 
+def square(x):
+  return x * x
+
+def calculator(a, b, operator):
+  if operator == '+':
+    return a + b
+  elif operator == '-':
+    return a - b
+  elif operator == '*':
+    return a * b
+  elif operator == '/':
+    return a / b
+  else:
+    return None
+
 def main():
-  print("Hello World!")
+  print(calculator(1, 2, '+'))
+  print(calculator(1, 2, '-'))
+  print(calculator(1, 2, '*'))
+  print(calculator(1, 2, '/'))
+  print(square(5))
   
 if __name__ == "__main__":
   main()
---
+++ practice/calculator_test.py
@@ -0,0 +1,13 @@
+from calculator import square
+
+def main():
+  test_square_basic()
+  
+def test_square_basic():
+  assert square(5) == 25
+  assert square(0) == 0
+  assert square(-5) == 25
+  assert square(2.5) == 6.25
+  
+if __name__ == "__main__":
+  main()
