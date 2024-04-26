8db933befc36d34a852db6aba6c363500b277831
John Dunn <johnwdunn20@gmail.com>
2024-04-25 18:26:22 -0700

    changes

--- practice/excel_password.py
+++ practice/excel_password.py
@@ -3,6 +3,27 @@
 
 import openpyxl
 
-wb = openpyxl.load_workbook('/Users/..', read_only=False)
+# Load the workbook
+workbook = openpyxl.load_workbook('path_to_your_file.xlsx')
 
-print(wb.sheetnames)
+# Access a specific worksheet
+worksheet = workbook['SheetName']
+
+# Check if the worksheet is protected
+if worksheet.protection.sheet:
+    print("The worksheet is protected.")
+else:
+    print("The worksheet is not protected.")
+
+# Attempting to modify a protected worksheet will raise an error unless it is unprotected
+try:
+    worksheet['A1'] = "New Value"  # This will fail if the worksheet is protected
+except AttributeError as e:
+    print("Failed to modify the worksheet:", e)
+
+# Unprotect the worksheet with the password
+worksheet.protection.disable()
+worksheet.password = None  # Clear the password if you wish
+
+# Now you should be able to edit the worksheet
+worksheet['A1'] = "New Value"
